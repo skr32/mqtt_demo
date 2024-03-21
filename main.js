9a237d8bb26b8cf51811284c05867da4dc85719e
SKR <sascha@krusbersky.net>
2024-03-21 19:29:02 +0100

    add files

---
+++ main.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create a GLTFLoader instance
+const loader = new GLTFLoader();
+
+// Load the glTF model
+loader.load(
+    // Path to the glTF file
+    'rpi3/scene.gltf',
+    // onLoad callback function
+    function (gltf) {
+        // Add the loaded model to the scene
+        scene.add(gltf.scene);
+    },
+    // onProgress callback function (optional)
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    // onError callback function (optional)
+    function (error) {
+        console.error('Error loading glTF model', error);
+    }
+);
+
+//camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotate the loaded model
+    scene.traverse((object) => {
+        if (object.isMesh) object.rotation.y += 0.01;
+    });
+
+    renderer.render(scene, camera);
+}
---
+++ mqtt.js
@@ -0,0 +1,33 @@
+import mqtt from 'mqtt';
+import { rotateScene } from './code.js';
+
+const host = "ws://m464ee61.emqx.cloud:8083/mqtt" 
+
+const options = {
+    // Clean session
+    clean: true,
+    connectTimeout: 4000,
+    // Authentication
+    clientId: 'web',
+    username: 'webfront',
+    password: 'Ehk@3r6RQ7$cSZ6RiNGK',
+  }
+  
+
+const client = mqtt.connect(host, options);
+
+
+client.on('connect', function () {
+    console.log('Connected to MQTT broker');
+    client.subscribe('rotation_data'); // Subscribe to the topic where rotation values are published
+});
+  
+  client.on('message', function (topic, message) {
+    // Message received on the subscribed topic
+    const data = JSON.parse(message.toString());
+    console.log(data);
+    if (data.x !== undefined && data.y !== undefined && data.z !== undefined) {
+        // Call the rotateScene function with received rotation values
+        rotateScene(data.x, data.y, data.z);
+    }
+});
